95b6386af4d39de334201e866625dd699044883a
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-05-21 13:08:24 +0200

    Heading staic ready

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ export interface HeaderProps { };
 const Header: FC<HeaderProps> = (props) => {
 
     return (
-        <div className=''>
+        <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
             <div className='flex justify-between bg-white max-w-6xl mx-5 xl:mx-auto'>
 
                 <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
@@ -47,9 +47,16 @@ const Header: FC<HeaderProps> = (props) => {
                     </div>
                 </div>
                 <div className='flex items-center justify-end space-x-4'>
-                    <HomeIcon className='navBtn' />
                     <MenuIcon className='h-6 md:hidden cursor-pointer' />
-                    <PaperAirplaneIcon className='navBtn' />
+                    <HomeIcon className='navBtn' />
+                    <div className='relative navBtn'>
+                        <div className='absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
+                        <PaperAirplaneIcon className='navBtn' />
+                    </div>
+                    <PlusCircleIcon className='navBtn' />
+                    <UserGroupIcon className='navBtn' />
+                    <HeartIcon className='navBtn' />
+                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFlfLM1b6ZQRQ/profile-displayphoto-shrink_200_200/0/1605088697876?e=1657152000&v=beta&t=AFl2DdQFWDilFk9Ii4W3hBZeLOa3KgbHGzWotmT72sc" alt="image" className='h-10 rounded-full cursor-pointer' />
                 </div>
             </div>
         </div>
