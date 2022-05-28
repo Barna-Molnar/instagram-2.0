59eacbc98e62b4a24af8a42d378f8b5f5535c1fc
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-05-28 16:45:56 +0200

    Custom Sign in page + redirect

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,12 +10,16 @@ import {
 } from '@heroicons/react/outline'
 
 import { HomeIcon } from '@heroicons/react/solid';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export interface HeaderProps { };
 
 
 const Header: FC<HeaderProps> = (props) => {
 
+    const { data: session } = useSession();
+
+    console.log({ session })
     return (
         <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
             <div className='flex justify-between bg-white max-w-6xl mx-5 xl:mx-auto'>
@@ -49,14 +53,29 @@ const Header: FC<HeaderProps> = (props) => {
                 <div className='flex items-center justify-end space-x-4'>
                     <MenuIcon className='h-6 md:hidden cursor-pointer' />
                     <HomeIcon className='navBtn' />
-                    <div className='relative navBtn'>
-                        <div className='absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
-                        <PaperAirplaneIcon className='navBtn' />
-                    </div>
-                    <PlusCircleIcon className='navBtn' />
-                    <UserGroupIcon className='navBtn' />
-                    <HeartIcon className='navBtn' />
-                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFlfLM1b6ZQRQ/profile-displayphoto-shrink_200_200/0/1605088697876?e=1657152000&v=beta&t=AFl2DdQFWDilFk9Ii4W3hBZeLOa3KgbHGzWotmT72sc" alt="image" className='h-10 rounded-full cursor-pointer' />
+                    {
+                        session ? (
+                            <>
+                                <div className='relative navBtn'>
+                                    <div className='absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
+                                        3
+                                    </div>
+                                    <PaperAirplaneIcon className='navBtn' />
+                                </div>
+                                <PlusCircleIcon className='navBtn' />
+                                <UserGroupIcon className='navBtn' />
+                                <HeartIcon className='navBtn' />
+                                <img
+                                    src={session?.user?.image!}
+                                    alt="profil picture"
+                                    className='h-10 rounded-full cursor-pointer'
+                                    onClick={() => signOut()}
+                                />
+                            </>
+                        ) : (
+                            <button onClick={() => signIn()}>Sign In</button>
+                        )
+                    }
                 </div>
             </div>
         </div>
