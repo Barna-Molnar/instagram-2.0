9de60ee4f3ab3c509c5097a3b14ab1e37eae445d
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-05-28 15:56:36 +0200

    Typed signIn

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -3,11 +3,11 @@ import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 
-const signIn = ({ providers }) => {
+const signIn = ({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   console.log({ providers });
   return (
     <>
-      {Object.values(providers).map((provider) => (
+      {Object.values(providers).map((provider: any) => (
         <div key={provider.name}>
           <button onClick={() => SignIntoProvider(provider.id)}>
             Sign in with {provider.name}
@@ -18,7 +18,7 @@ const signIn = ({ providers }) => {
   );
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const providers = await getProviders();
   return {
     props: { providers },
