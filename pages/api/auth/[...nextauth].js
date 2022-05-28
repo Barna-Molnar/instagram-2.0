59b7362b52a23444f1110fa2a0be1d6b296141a7
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-05-28 17:27:30 +0200

    Update session calback fn

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth, { Awaitable, Session, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import GoogleProvider from "next-auth/providers/google";
+
+
+export default NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.NEXTAUTH_SECRET,
+        }),
+        // ...add more providers here
+    ],
+
+    pages: {
+        signIn: "/auth/signin",
+    },
+    callbacks: {
+        async session({ session, user, token }) {
+            session.user.username = session.user.name.split("").join("").toLocaleLowerCase();
+            session.user.uid = token.sub;
+            return session;
+        }
+    }
+});
