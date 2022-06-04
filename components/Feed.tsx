59ebea3698a95ba538c74efe0f96603fca95f246
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-06-04 18:32:51 +0200

    Show signed in user in stories

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/react';
 import { FC } from 'react';
 import MiniProfile from './MiniProfile';
 import Posts from './Posts';
@@ -8,19 +9,22 @@ export interface FeedProps { };
 
 const Feed: FC<FeedProps> = (props) => {
 
+    const { data: session } = useSession();
+
     return (
-        <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+        <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && '!grid-cols-1 !max-w-3xl '}`}>
             <section className='col-span-2'>
                 <Stories />
                 <Posts />
             </section>
-
-            <section className='hidden xl:inline-grid md:col-span-1'>
-                <div className='fixed top-20'>
-                    <MiniProfile />
-                    <Suggestions />
-                </div>
-            </section>
+            {session && (
+                <section className='hidden xl:inline-grid md:col-span-1'>
+                    <div className='fixed top-20'>
+                        <MiniProfile />
+                        <Suggestions />
+                    </div>
+                </section>
+            )}
         </main>
     );
 };
--- components/Stories.tsx
+++ components/Stories.tsx
@@ -2,15 +2,25 @@ import { FC, useEffect, useState } from 'react';
 import faker from '@faker-js/faker';
 import Story from './Story';
 import { useUsers } from '../customHooks/useSuggestions';
+import { useSession } from 'next-auth/react';
 
 export interface StoriesProps { };
 
 const Stories: FC<StoriesProps> = (props) => {
 
     const { suggestedUsers } = useUsers(20)
+    const { data: session } = useSession()
 
     return (
         <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+            {
+                session && (
+                    <Story
+                        img={session.user.image!}
+                        username={session.user.username!}
+                    />
+                )
+            }
             {suggestedUsers?.map((user, index) => (
                 <Story
                     key={index}
